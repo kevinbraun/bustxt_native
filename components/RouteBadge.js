89f24ec3ab1570589a38692165b281ce85979605
Kevin Braun <kevin@kbraun.ca>
2020-02-22 14:18:53 -0600

    Upgrading Expo, tweaking some route badge styles.

--- components/RouteBadge.js
+++ components/RouteBadge.js
@@ -1,18 +1,29 @@
 import React from 'react'
 import {StyleSheet} from "react-native";
 import { Text } from 'native-base';
+import { View } from 'native-base';
 
 const styles = StyleSheet.create({
-  badge: {
+  badgeContainer: {
+    width: 40,
+    height: 40,
+    
     borderColor: 'black',
     borderRadius: 3,
     borderStyle: 'solid',
     borderWidth: 1,
 
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  badge: {
+
     fontWeight: 'bold',
     fontSize: 18,
 
-    padding: 3,
+    padding: 2,
     textAlign: 'center',
   }
 });
@@ -21,9 +32,11 @@ const RouteBadge = props => {
   let {route} = props;
 
   return (
-    <Text style={styles.badge}>
-      {route.number}
-    </Text>
+    <View style={styles.badgeContainer} >
+      <Text style={styles.badge}>
+        {route.number}
+      </Text>
+    </View>
   )
 }
 
